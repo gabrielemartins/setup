e757a38647a99bded9d7a6245d0edf36c12560f6
Gabriele <gabriieele6@gmail.com>
2021-08-27 12:58:34 -0300

    feat: vscode and extensions installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 PURPLE='\033[1;34m'
 GREEN='\033[1;32m'
+LIGHT_GREEN='\033[0;32]'
+BLUE='\033[0;36m'
 NO_COLOR='\033[0m'
 
 echo -e "${PURPLE}Hi! My name's Hysac I'm going to help you to setup the applications in the computer.${NO_COLOR}"
@@ -38,3 +40,73 @@ sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo -e "${GREEN}oh-my-zsh installation finished.${NO_COLOR}"
 clear
+
+echo -e ${PURPLE}"I'm going to install Visual Studio Code now.${NO_COLOR}"
+sudo apt install software-properties-common apt-transport-https
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
+echo -e "${GREEN}Visual Studio Code installation finished.${NO_COLOR}"
+clear
+
+echo -e ${PURPLE}"I'm going to install Visual Studio Code extensions now.${NO_COLOR}"
+echo -e "${BLUE}- 2077 theme${NO_COLOR}"
+code --install-extension Endormi.2077-theme
+echo -e "${LIGHT_GREEN}2077 theme installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Live Share${NO_COLOR}"
+code --install-extension MS-vsliveshare.vsliveshare
+echo -e "${LIGHT_GREEN}Live Share installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Live Share Audio${NO_COLOR}"
+code --install-extension MS-vsliveshare.vsliveshare-audio
+echo -e "${LIGHT_GREEN}Live Share Audio installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Live Server${NO_COLOR}"
+code --install-extension ritwickdey.LiveServer
+echo -e "${LIGHT_GREEN}Live Server installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Docker${NO_COLOR}"
+code --install-extension ms-azuretools.vscode-docker
+echo -e "${LIGHT_GREEN}Docker installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Material Icon Theme${NO_COLOR}"
+code --install-extension PKief.material-icon-theme
+echo -e "${LIGHT_GREEN}Material Icon Theme installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- :emojisense:${NO_COLOR}"
+code --install-extension bierner.emojisense
+echo -e "${LIGHT_GREEN}:emojisense: installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Color Picker${NO_COLOR}"
+code --install-extension anseki.vscode-color
+echo -e "${LIGHT_GREEN}Color Picker installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Colorize${NO_COLOR}"
+code --install-extension kamikillerto.vscode-colorize
+echo -e "${LIGHT_GREEN}Colorize installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- DotENV${NO_COLOR}"
+code --install-extension mikestead.dotenv
+echo -e "${LIGHT_GREEN}DotENV installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Bracket Pair Colorizer 2${NO_COLOR}"
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+echo -e "${LIGHT_GREEN}Bracket Pair Colorizer 2 installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Prettier${NO_COLOR}"
+code --install-extension esbenp.prettier-vscode
+echo -e "${LIGHT_GREEN}Prettier installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Visual Studio IntelliCode${NO_COLOR}"
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+echo -e "${LIGHT_GREEN}Visual Studio IntelliCode installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- Code Runner${NO_COLOR}"
+code --install-extension ormulahendry.code-runner
+echo -e "${LIGHT_GREEN}Code Runner installation finished.${NO_COLOR}"
+
+echo -e "${BLUE}- GitLens${NO_COLOR}"
+code --install-extension eamodio.gitlens
+echo -e "${LIGHT_GREEN}GitLens installation finished.${NO_COLOR}"
