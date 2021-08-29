8ebeea9c23874159a9f29b6dbef311dc89da332e
Gabriele <gabriieele6@gmail.com>
2021-08-29 16:35:51 -0300

    fix: change vscode installation to snap

--- setup.sh
+++ setup.sh
@@ -57,11 +57,7 @@ echo -e "${GREEN}NVM installation finished.${NO_COLOR}"
 clear
 
 echo -e ${PURPLE}"I'm going to install Visual Studio Code now.${NO_COLOR}"
-sudo apt install software-properties-common apt-transport-https
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt install code
+sudo snap install --classic code
 echo -e "${GREEN}Visual Studio Code installation finished.${NO_COLOR}"
 clear
 
