378f253d8c05b0e34deeb51a1e1a492167a1cba7
Gabriele <gabriieele6@gmail.com>
2021-08-29 16:45:23 -0300

    feat: add docker installation

--- setup.sh
+++ setup.sh
@@ -56,6 +56,25 @@ source ~/.zshrc
 echo -e "${GREEN}NVM installation finished.${NO_COLOR}"
 clear
 
+echo -e ${PURPLE}"I'm going to install Docker now.${NO_COLOR}"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+apt-transport-https \
+    ca-certificates \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+echo -e "${GREEN}Docker installation finished.${NO_COLOR}"
+clear
+
+
 echo -e ${PURPLE}"I'm going to install Visual Studio Code now.${NO_COLOR}"
 sudo snap install --classic code
 echo -e "${GREEN}Visual Studio Code installation finished.${NO_COLOR}"
---
+++ test.sh
@@ -0,0 +1,6 @@
+PURPLE='\033[1;34m'
+GREEN='\033[1;32m'
+BLUE='\033[0;36m'
+NO_COLOR='\033[0m'
+
+echo -e "${GREEN}Hello, world!"
